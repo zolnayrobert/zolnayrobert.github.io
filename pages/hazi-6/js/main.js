1f069f9679555e2a53f4dcecc8d768251d583156
Robert Zolnay <zolnayrobert@gmail.com>
2023-12-15 19:09:16 +0100

    S

--- pages/hazi-6/js/main.js
+++ pages/hazi-6/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 { 
-console.log("v.1847");    //verzió szám
+console.log("v.1848");    //verzió szám
     //Kapcsolat űrlap
     $('#contactForm').submit(function (e)
     {
