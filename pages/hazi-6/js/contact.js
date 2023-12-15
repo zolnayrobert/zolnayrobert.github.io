60d7da9b9197dc266551cb016956496c942890f7
Robert Zolnay <zolnayrobert@gmail.com>
2023-12-15 10:48:06 +0100

    X

--- pages/hazi-6/js/contact.js
+++ pages/hazi-6/js/contact.js
@@ -1,99 +1,72 @@
 $(document).ready(function()
 { 
-/*    //Küldés gomb megnyomására visszajelzés
-    $('#contact-submit').submit(function(e){
+    const frm = $('#contactForm');
 
+    frm.submit(function (e)
+    {
+        //Spinner mutatása válaszig
+        document.getElementById("loading-overlay").style.display = "flex";
         e.preventDefault();
-
-        //adatok összegyűjtése küldéshez
-        let datas = {
-            name: $("#name").val(),
-            email: $("#email").val(),
-            message: $("#message").val(),
-        };
-console.log(datas);  
-
-        //elküldjük az adatokat
+    
         $.ajax({
-            url: "https://formsubmit.co/ajax/520a354098799bbb56777753e910654f",
+            type: frm.attr('method'),
             method: "POST",
-            data: datas,
-            dataType: 'json',
+            url: "https://formsubmit.co/ajax/520a354098799bbb56777753e910654f",
+            dataType: 'html',
             accepts: 'application/json',
-            success: function() {
-                sendresponse("success");
+            data: frm.serialize(),
+            success: function (response) {        
+                //sikerres küldés
+                sMsg(response);
             },
-            error: function() {sendresponse("error");}
+            error: function (data) {
+                eMsg();
+            }
         });
-
-/*
-        $.post("https://formsubmit.co/520a354098799bbb56777753e910654f", datas, function() {});
-        // Jelenítse meg az üzenetet a küldésről
-        document.getElementById("form-message-report").style.display = "block";   
-        // 2 másodperc késleltetéssel
-        setTimeout(function() {
-            // Rejtse el az üzentet
-            document.getElementById("form-message-report").style.display = "none";
-        }, 2000);
-       
-    });
-
-   */ 
-
-const frm = $('#contactForm');
-const msg_res ='';
-
-frm.submit(function (e)
-{
-    e.preventDefault();
-   
-    $.ajax({
-        type: frm.attr('method'),
-        method: "POST",
-        url: "https://formsubmit.co/ajax/520a354098799bbb56777753e910654f",
-        dataType: 'html',
-        accepts: 'application/json',
-        data: frm.serialize(),
-        success: function (response) {
-console.log(response);          
-        $("#form-message-report").html(sendresponse(response));
-            if(response != msg_res){
-//                msg_res = response; //store new response
-              }
-        },
-        error: function (data) {
-            console.log('An error occurred.');
-            console.log(data);
-        }
-    });
-    return false; // here a change
-}); 
+        return false; // here a change
+    }); 
 });
 
-function sendresponse(e){
-console.log(e);
+//sikeres válasz feldolgozása
+function sMsg(e)
+{
     let msg = JSON.parse(e);
     if(msg.success === true) {
+        //Spinner elrejtése
+        document.getElementById("loading-overlay").style.display = "none";
+
         // Jelenítse meg az üzenetet a küldésről
+        $("#form-message-report").text("Köszönjük leveled, hamarosan válaszolunk!");
         document.getElementById("form-message-report").style.display = "block"; 
 
-        // 2 másodperc késleltetéssel
+        // 5 másodperc késleltetéssel
         setTimeout(function() {
-            // Rejtse el az üzentet
+            // Rejtse el az üzenetet
             document.getElementById("form-message-report").style.display = "none";
-        }, 2000);
+        }, 5000);
     } else {
-        $("#form-message-report").text("Sajnos valami hiba történt, kérjük próbálja meg később!");
-        
-        // Jelenítse meg az üzenetet a küldésről
-        document.getElementById("form-message-report").style.color = "red"; 
-        document.getElementById("form-message-report").style.display = "block"; 
-
-        // 2 másodperc késleltetéssel
-        setTimeout(function() {
-            // Rejtse el az üzentet
-            document.getElementById("form-message-report").style.display = "none";
-        }, 2000);
+        eMsg();
     }
 }
+
+//ha hibára futna is adunk választ
+function eMsg()
+{
+    //Spinner elrejtése
+    document.getElementById("loading-overlay").style.display = "none";
+    
+    //hibaüzenet szövege
+    $("#form-message-report").text("Sajnos valami hiba történt, kérjük próbálja meg később!");
+        
+    // Jelenítse meg az üzenetet a küldésről
+    document.getElementById("form-message-report").style.color = "red"; //piros színnel jelezzük a hibát
+    document.getElementById("form-message-report").style.display = "block"; //hiba megjelenítése
+
+    // 5 másodperc késleltetéssel
+    setTimeout(function() {
+        // Rejtse el az üzenetet
+        document.getElementById("form-message-report").style.display = "none"; //üzenet eltüntetése
+    }, 5000);
+}
+
         
